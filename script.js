bd0c413c19bc2ab3b52fbfd541a8be744345cb01
karthik2628 <karthikkishore2603@gmail.com>
2023-02-15 20:03:58 +0530

    home

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 //----------------------NAVBAR----------------------//
 
 const nav = document.querySelector(".nav"),
-  searchIcon = document.querySelector("#searchIcon"),
   navOpenBtn = document.querySelector(".navOpenBtn"),
   navCloseBtn = document.querySelector(".navCloseBtn");
 
-searchIcon.addEventListener("click", () => {
-  nav.classList.toggle("openSearch");
-  nav.classList.remove("openNav");
-  if (nav.classList.contains("openSearch")) {
-    return searchIcon.classList.replace("uil-search", "uil-times");
-  }
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
+
 
 navOpenBtn.addEventListener("click", () => {
   nav.classList.add("openNav");
